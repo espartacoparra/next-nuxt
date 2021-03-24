4faea098b9113712bc0ee2b63b45b71ed0d74fa7
espartacoparra <espartaco.ing@gmail.com>
2021-03-24 17:21:23 -0400

    init project

---
+++ nuxtapp/layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider
+      v-model="collapsed"
+      :trigger="null"
+      collapsible
+      :style="{ overflow: 'auto', height: 'auto', left: 0 }"
+    >
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1" @click="move('/')">
+          <a-icon type="user" />
+          <span>Home</span>
+        </a-menu-item>
+        <a-menu-item key="2" @click="move('/users')">
+          <a-icon type="video-camera" />
+          <span>Users</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        <Nuxt />
+      </a-layout-content>
+      <a-layout-footer :style="{ textAlign: 'center' }">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+  methods: {
+    move(path) {
+      this.$router.push({ path: path, query: { q: this.q } });
+    },
+  },
+};
+</script>
+<style>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
---
+++ nuxtapp/pages/users.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>a</div>
+</template>
+
+<script>
+export default {
+  name: "user",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    async getUsers() {
+      const ip = await this.$axios.$get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      console.log(ip);
+      this.ip = ip;
+    },
+  },
+  created() {
+    this.getUsers();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
